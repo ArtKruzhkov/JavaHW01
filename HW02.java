a3d79e3d514a6d048987f0d670b1f3b33c7bd386
Artem <artkruzhkov@gmail.com>
2023-02-12 00:13:06 +0300

    HW02final

--- HW02.java
+++ HW02.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class HW02 {
     public static void main(String[] args) {
         String json = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
@@ -30,6 +34,7 @@ public class HW02 {
         sb1.append(studentmark[1]);
         sb1.append(" по предмету ");
         sb1.append(studentsubject[1] + ".");
+        String print1 = sb1.toString();
 
         StringBuilder sb2 = new StringBuilder();
         sb2.append("Студент ");
@@ -38,6 +43,7 @@ public class HW02 {
         sb2.append(studentmark2[1]);
         sb2.append(" по предмету ");
         sb2.append(studentsubject2[1] + ".");
+        String print2 = sb2.toString();
 
         StringBuilder sb3 = new StringBuilder();
         sb3.append("Студент ");
@@ -46,10 +52,51 @@ public class HW02 {
         sb3.append(studentmark3[1]);
         sb3.append(" по предмету ");
         sb3.append(studentsubject3[1] + ".");
+        String print3 = sb3.toString();
 
         System.out.println("Итоговые строки: ");
-        System.out.println(sb1);
-        System.out.println(sb2);
-        System.out.println(sb3);
+        System.out.println(print1);
+        System.out.println(print2);
+        System.out.println(print3);
+
+        CreateFile();
+        WriteFile(print1);
+        WriteFile(print2);
+        WriteFile(print3);
+    }
+
+    static public void WriteFile(String s) {
+        try(FileWriter writer = new FileWriter("newFile.txt", true)) {
+            writer.write(s);
+            writer.append('\n');
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static public String TextPrint(String s) {
+        String res = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            sb.append(s);
+        }
+        res = sb.toString();
+        return res;
+    }
+    static public void CreateFile() {
+        try {
+            String pathProject = System.getProperty("user.dir");
+            String pathFile = pathProject.concat("/newFile.txt");
+            File newFile = new File(pathFile);
+            if (newFile.createNewFile()) {
+                System.out.println("file.created");
+            }
+            else {
+                System.out.println("file.existed");
+            }
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");}
     }
 }
